1f27800aa94bb46476b48bb52fd5c7dfb29f840e
CarlyWenig <69254490+CarlyWenig@users.noreply.github.com>
2020-08-05 11:33:33 -0400

    My first Xcode Project

---
+++ kwk/StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Carly Wenig on 8/5/20.
+//  Copyright © 2020 Carly Wenig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   print("This is a print statement--testing--testing")
+    }
+
+
+}
+
